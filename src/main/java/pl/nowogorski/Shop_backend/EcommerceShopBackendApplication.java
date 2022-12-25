14d3b89d2ff192739ba9781aabd494270a320359
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-25 19:51:12 +0100

    First commit

---
+++ src/main/java/pl/nowogorski/Shop_backend/EcommerceShopBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.nowogorski.Shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/nowogorski/Shop_backend/EcommerceShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nowogorski.Shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
