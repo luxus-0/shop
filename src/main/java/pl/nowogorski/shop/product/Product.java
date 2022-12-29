7f89c1b0352fbb0ab93a70910dc2f2ddf131b3ad
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-29 22:25:45 +0100

    Change

--- src/main/java/pl/nowogorski/shop/product/Product.java
+++ src/main/java/pl/nowogorski/shop/product/Product.java
@@ -2,10 +2,11 @@ package pl.nowogorski.shop.product;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
-record Product(@Id @GeneratedValue Long id, String name, String category,
-               String description, BigDecimal price, String currency) {
+record Product(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id,
+               String name, String category, String description, BigDecimal price, String currency) {
 }
