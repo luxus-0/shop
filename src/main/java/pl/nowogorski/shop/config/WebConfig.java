05243748dea551adba8012e6e117383c707b10da
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-31 01:26:15 +0100

    change name package from configuration to config

--- src/main/java/pl/nowogorski/shop/config/WebConfig.java
+++ src/main/java/pl/nowogorski/shop/config/WebConfig.java
@@ -1,4 +1,4 @@
-package pl.nowogorski.shop.configuration;
+package pl.nowogorski.shop.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
