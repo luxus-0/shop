f5955072ac0f4932619caace19b9dce0096fb254
Łukasz Nowogórski <luxus0@gmail.com>
2023-01-03 16:45:48 +0100

    Add test for slugify method

--- src/test/java/pl/nowogorski/shop/admin/UploadedFileNameUtilsTest.java
+++ src/test/java/pl/nowogorski/shop/admin/UploadedFileNameUtilsTest.java
@@ -1,12 +1,30 @@
 package pl.nowogorski.shop.admin;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UploadedFileNameUtilsTest {
 
-    @BeforeEach
-    void setUp() {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "product test.png, product-test.png",
+            "hello file.png, hello-file.png",
+            "ąęśćżźńłó.png, aesczznlo.png",
+            "Product 1.png, product-1.png",
+            "Product - 1.png, product-1.png",
+            "Product_1.png, product-1.png",
+            "Product__1.png, product-1.png"
+    })
+    void shouldReturnSlugifyFileNameWhenInputFileName(String inputFileName, String outputFileName){
+        //given
+        UploadedFileNameUtils uploadedFileNameUtils = new UploadedFileNameUtils();
+        //when
+        String fileName = uploadedFileNameUtils.generateSlugFromString(inputFileName);
+        //then
+        assertEquals(fileName, outputFileName);
     }
 }
