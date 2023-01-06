973cecf4a3430de9b745d2c3dedc81e49398e463
= <luxus0@gmail.com>
2023-01-06 17:14:52 +0100

    Add method deleteCartById(Long id);

--- src/main/java/pl/nowogorski/shop/admin/cart/CartRepository.java
+++ src/main/java/pl/nowogorski/shop/admin/cart/CartRepository.java
@@ -1,10 +1,15 @@
 package pl.nowogorski.shop.admin.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findByCreatedLessThan(LocalDateTime minusDays);
+    @Query("delete from Cart c where c.id =: id")
+    @Modifying
+    void deleteCardById(Long id);
 }
