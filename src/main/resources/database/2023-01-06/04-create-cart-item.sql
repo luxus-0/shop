2fd881528cff83c054c456c17ecb871bca0b907e
= <luxus0@gmail.com>
2023-01-06 17:18:55 +0100

    Change field: FROM cart_id bigint null TO cart_id bigint not null
    Change FROM null TO not null.

--- src/main/resources/database/2023-01-06/04-create-cart-item.sql
+++ src/main/resources/database/2023-01-06/04-create-cart-item.sql
@@ -4,7 +4,7 @@ create table if not exists cart_item(
     id bigint not null primary key,
     product_id bigint not null,
     quantity int,
-    cart_id bigint null,
+    cart_id bigint not null,
     constraint fk_cart_item_product_id foreign key (product_id) references product(id),
     constraint fk_cart_item_cart_id foreign key (cart_id) references cart(id)
 );
