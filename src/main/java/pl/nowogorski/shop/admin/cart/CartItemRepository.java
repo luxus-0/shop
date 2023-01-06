419408ae01de13dd1b5a48887a177ef608486321
= <luxus0@gmail.com>
2023-01-06 17:14:39 +0100

    Add method deleteByCartId(Long id);

--- src/main/java/pl/nowogorski/shop/admin/cart/CartItemRepository.java
+++ src/main/java/pl/nowogorski/shop/admin/cart/CartItemRepository.java
@@ -1,7 +1,12 @@
 package pl.nowogorski.shop.admin.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Long countByCartId(Long cardId);
+    @Query("delete from CartItem ci where ci.cartId =: cartId")
+    @Modifying
+    void deleteByCartId(Long id);
 }
