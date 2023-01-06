624ec6cc5b6f4ce15b5c4f9b865e16844e414b17
= <luxus0@gmail.com>
2023-01-06 17:34:44 +0100

    Add methods: void deleteByCartId(Long id); delete cart item by id
    void deleteAllByCardIdIn(List<Long> cartId);
    delete all card item id in specific id
    
    This two function work the same, but it is another query

--- src/main/java/pl/nowogorski/shop/admin/cart/CartItemRepository.java
+++ src/main/java/pl/nowogorski/shop/admin/cart/CartItemRepository.java
@@ -4,9 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Long countByCartId(Long cardId);
     @Query("delete from CartItem ci where ci.cartId =: cartId")
     @Modifying
     void deleteByCartId(Long id);
+
+    @Query("delete from CartItem ci where ci.cartId in (:cartId)")
+    @Modifying
+    void deleteAllByCardIdIn(List<Long> cartId);
 }
