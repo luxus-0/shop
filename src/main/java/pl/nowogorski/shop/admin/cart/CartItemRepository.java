aeca960682431cb2be0507043f8562d69145b336
= <luxus0@gmail.com>
2023-01-06 15:25:20 +0100

    add method declaration: Long countByCartId(Long cartId)

--- src/main/java/pl/nowogorski/shop/admin/cart/CartItemRepository.java
+++ src/main/java/pl/nowogorski/shop/admin/cart/CartItemRepository.java
@@ -3,4 +3,5 @@ package pl.nowogorski.shop.admin.cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Long countByCartId(Long cardId);
 }
