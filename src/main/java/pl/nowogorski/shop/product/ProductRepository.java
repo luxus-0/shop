565a5666a3e46c014810435377af66dab4727010
= <luxus0@gmail.com>
2023-01-06 13:52:07 +0100

    Add cart module

--- src/main/java/pl/nowogorski/shop/product/ProductRepository.java
+++ src/main/java/pl/nowogorski/shop/product/ProductRepository.java
@@ -3,6 +3,7 @@ package pl.nowogorski.shop.product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
