e2971f2b99b7617ff59ea60ec2a36dcc826d85a6
= <luxus0@gmail.com>
2023-01-06 21:38:16 +0100

    Add class Order with id, LocalDateTime, enum OrderStatus, List<OrderRows> orderRows... Add Client personal data: fistName, lastName, street, zipCode, city, email..
    We must separate order and client, now is not good option...

---
+++ src/main/java/pl/nowogorski/shop/order/Order.java
@@ -0,0 +1,45 @@
+package pl.nowogorski.shop.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "'order'")
+class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime placeDate;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany
+    @JoinColumn(name = "orderId")
+    private List<OrderRow> orderRows;
+    private BigDecimal grossAmount;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String email;
+    private String phone;
+}
