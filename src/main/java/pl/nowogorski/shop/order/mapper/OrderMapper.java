e66391a49b91e787dc4a0c7f0ef88aeb6b776d54
Łukasz Nowogórski <nowogorski.lukasz0@gmail.com>
2023-01-28 18:20:09 +0100

    Add static method :  Order createNewOrder(OrderDto) add builder with field: customers(customers) add method createCustomer - builder Customer with fields: firstName, lastName, street, zipCode, city, email, phone

--- src/main/java/pl/nowogorski/shop/order/mapper/OrderMapper.java
+++ src/main/java/pl/nowogorski/shop/order/mapper/OrderMapper.java
@@ -3,6 +3,7 @@ package pl.nowogorski.shop.order.mapper;
 import org.springframework.stereotype.Service;
 import pl.nowogorski.shop.admin.cart.Cart;
 import pl.nowogorski.shop.admin.cart.CartItem;
+import pl.nowogorski.shop.customer.Customer;
 import pl.nowogorski.shop.order.Order;
 import pl.nowogorski.shop.order.OrderDto;
 import pl.nowogorski.shop.order.OrderRow;
@@ -13,18 +14,32 @@ import pl.nowogorski.shop.shipment.Shipment;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
 import static java.time.LocalDateTime.now;
 
 @Service
 public class OrderMapper {
-    public static Order createNewOrder(OrderDto customer, Cart cart, Shipment shipment, Payment payment) {
+    public static Order createNewOrder(OrderDto orderDto, Cart cart, Shipment shipment, Payment payment) {
+        Set<Customer> customers = Set.of(createCustomer(orderDto));
         return Order.builder()
-                .customerId(customer.getId())
+                .customers(customers)
                 .placeDate(now())
                 .orderStatus(OrderStatus.NEW)
                 .grossAmount(calculateGrossAmount(cart.getItems(), shipment))
                 .payment(payment)
+                .build();
+    }
+
+    private static Customer createCustomer(OrderDto orderDto) {
+        return Customer.builder()
+                .firstName(orderDto.getFirstName())
+                .lastName(orderDto.getLastName())
+                .street(orderDto.getStreet())
+                .zipCode(orderDto.getZipCode())
+                .city(orderDto.getCity())
+                .email(orderDto.getEmail())
+                .phone(orderDto.getPhone())
                 .build();
     }
 
