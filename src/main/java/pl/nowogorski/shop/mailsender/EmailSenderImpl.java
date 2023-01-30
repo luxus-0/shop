1c056a26afb0dc66053834ebdc6c4a3f1089575c
Łukasz Nowogórski <nowogorski.lukasz0@gmail.com>
2023-01-30 20:55:03 +0100

    Class implementation for EmailSender with method sending message email

--- src/main/java/pl/nowogorski/shop/mailsender/EmailSenderImpl.java
+++ src/main/java/pl/nowogorski/shop/mailsender/EmailSenderImpl.java
@@ -1,16 +1,20 @@
 package pl.nowogorski.shop.mailsender;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class MailSenderImpl {
+public class EmailSenderImpl implements EmailSender {
 
     private final JavaMailSender mailSender;
-    public void send(String to, String subject, String text){
+
+    public EmailSenderImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void send(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("luxus0@gmail.com");
         message.setTo(to);
