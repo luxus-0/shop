39a81b3944cca148afe96c4ee82eada2938f815a
Łukasz Nowogórski <luxus0@gmail.com>
2023-01-31 17:16:59 +0100

    change name package

--- src/main/java/pl/nowogorski/shop/order/OrderController.java
+++ src/main/java/pl/nowogorski/shop/order/OrderController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.nowogorski.shop.order.dto.OrderDto;
 import pl.nowogorski.shop.payment.PaymentService;
 import pl.nowogorski.shop.shipment.ShipmentImpl;
 
