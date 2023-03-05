6282e66f32e9ddfde8ff4115961430ff3ecbf694
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-06 00:01:38 +0100

    Change in Web Config Email bean Name FakeEmailService

--- src/main/java/pl/nowogorski/shop/infrastructure/configuration/WebConfig.java
+++ src/main/java/pl/nowogorski/shop/infrastructure/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package pl.nowogorski.shop.config;
+package pl.nowogorski.shop.infrastructure.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
