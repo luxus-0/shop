5c39098908f64bf534d5c5bb4b402d346ee630fd
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-06 16:01:27 +0100

    Add methods:  -updateOrder(id, @RequestBody Map<String, String> values)  -private void pathValues(AdminOrder, Map<String,String>)  check OrderStatus not null and set OrderStatus NEW

--- src/main/java/pl/nowogorski/shop/admin/order/AdminOrderService.java
+++ src/main/java/pl/nowogorski/shop/admin/order/AdminOrderService.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +25,17 @@ public class AdminOrderService {
 
     AdminOrder getOrder(Long id){
         return adminOrderRepository.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    public void updateOrder(Long id, Map<String, String> values) {
+        AdminOrder adminOrder = adminOrderRepository.findById(id).orElseThrow();
+        patchValues(adminOrder, values);
+    }
+
+    private void patchValues(AdminOrder adminOrder, Map<String, String> values) {
+        if(values.get("orderStatus") != null){
+            adminOrder.setAdminOrderStatus(AdminOrderStatus.valueOf(values.get("orderStatus")));
+        }
     }
 }
