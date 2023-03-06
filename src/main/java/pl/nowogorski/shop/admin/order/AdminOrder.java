2c72338f8b5b75758c509e9f8ad1a66fa8b2a78d
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-06 14:43:08 +0100

    Add entity class AdminOrder with fields: id, LocalDate placeDate, AdminOrderStatus(enum), List<AdminOrderRow> orderRows, BigDecimal grossValue, @OneToOne Customer, @OneToOne AdminPayment

---
+++ src/main/java/pl/nowogorski/shop/admin/order/AdminOrder.java
@@ -0,0 +1,37 @@
+package pl.nowogorski.shop.admin.order;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import pl.nowogorski.shop.admin.payment.AdminPayment;
+import pl.nowogorski.shop.customer.Customer;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "'order'")
+@Getter
+public class AdminOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate placeDate;
+
+    @Enumerated(EnumType.STRING)
+    private AdminOrderStatus adminOrderStatus;
+
+    @OneToMany
+    @JoinColumn(name = "orderId")
+    private List<AdminOrderRow> orderRows;
+
+    private BigDecimal grossValue;
+
+    @OneToOne
+    private Customer customer;
+
+    @OneToOne
+    private AdminPayment adminPayment;
+}
