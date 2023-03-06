69e0b7b0e4f63d754d4a2fb0b7ca958a23ca8202
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-06 01:52:52 +0100

    Change test method

--- src/test/java/pl/nowogorski/shop/order/OrderImplTest.java
+++ src/test/java/pl/nowogorski/shop/order/OrderImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.nowogorski.shop.admin.cart.Cart;
 import pl.nowogorski.shop.admin.cart.CartItem;
